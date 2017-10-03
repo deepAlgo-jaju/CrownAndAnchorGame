6ca646a97aa3f7a1ffe0032f3abf96c981ae544d
muhammadjunaidiftikhar@gmail.com <alpha861>
2017-10-03 15:55:06 +0500

    Bug # 1 Solved

--- src/src/Player.java
+++ src/src/Player.java
@@ -41,7 +41,6 @@ public class Player {
 		if (winnings < 0) throw new IllegalArgumentException("Winnings cannot be negative.");
 		balance = balance + winnings;
 	}
-	
 	public String toString() {
 		return String.format("Player: %s, Balance: %d, Limit: %d", name, balance, limit);
 	}
