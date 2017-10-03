6f3361f7ccf84862eefffe6f0330b76c56330035
muhammadjunaidiftikhar@gmail.com <alpha861>
2017-10-03 16:15:10 +0500

    Bug # 2

--- src/src/Player.java
+++ src/src/Player.java
@@ -16,6 +16,7 @@ public class Player {
 	public String getName() { return name; }
 	public int getBalance() { return balance; }
 	public int getLimit() { return limit; }
+
 	public void setLimit(int limit) {
 		if (limit < 0) throw new IllegalArgumentException("Limit cannot be negative.");
 		if (limit > balance)  throw new IllegalArgumentException("Limit cannot be greater than balance.");
