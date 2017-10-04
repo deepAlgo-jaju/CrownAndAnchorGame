fe6ac149e01523bd1eebb489dbaaf41d3256b927
muhammadjunaidiftikhar@gmail.com <alpha861>
2017-10-05 04:49:47 +0500

    Bug # 3

--- src/src/Main.java
+++ src/src/Main.java
@@ -13,6 +13,7 @@ public class Main {
         Game game = new Game(d1, d2, d3);
 
         List<DiceValue> cdv = game.getDiceValues();
+
         int totalWins = 0;
         int totalLosses = 0;
         while (true)
