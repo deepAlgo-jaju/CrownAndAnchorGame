90ae44ce7cf3327fe6a2e0cf4a92ca5baf762a86
muhammadjunaidiftikhar@gmail.com <alpha861>
2017-10-05 04:51:04 +0500

    Bug # 3

--- src/src/Game.java
+++ src/src/Game.java
@@ -19,7 +19,7 @@ public class Game {
 	public List<DiceValue> getDiceValues() {
 		values.clear();
 		for (Dice d : dice) {
-			values.add(d);
+			values.add(d.roll());
 		}
 		return Collections.unmodifiableList(values);
 	}
