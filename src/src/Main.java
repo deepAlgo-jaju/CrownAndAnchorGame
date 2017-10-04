c3fcc1b4f5800ce8d44ee894cf49280321560ffa
muhammadjunaidiftikhar@gmail.com <alpha861>
2017-10-05 04:50:14 +0500

    Bug # 3

--- src/src/Main.java
+++ src/src/Main.java
@@ -11,7 +11,6 @@ public class Main {
 	    Dice d3 = new Dice();
         Player player = new Player("Fred", 100);
         Game game = new Game(d1, d2, d3);
-
         List<DiceValue> cdv = game.getDiceValues();
         int totalWins = 0;
         int totalLosses = 0;
