a30a46a19dfe0b6d3d14e59fc9e3b768fa8d595e
muhammadjunaidiftikhar@gmail.com <alpha861>
2017-10-05 06:00:46 +0500

    Bug # 6

--- src/src/Main.java
+++ src/src/Main.java
@@ -18,7 +18,7 @@ public class Main {
         {
             int winCount = 0;
             int loseCount = 0;
-            for (int i = 1; i < 100; i++)
+            for (int i = 0; i < 100; i++)
             {
                 String name = "Fred";
                 int balance = 100;
