63998c75b8663ccc8ea01d01c1e50e0c85af7463
muhammadjunaidiftikhar@gmail.com <alpha861>
2017-10-05 05:24:04 +0500

    Bug # 4 solved

--- src/src/Game.java
+++ src/src/Game.java
@@ -23,7 +23,6 @@ public class Game {
 	}
 
 	public int playRound(Player player, DiceValue pick, int bet ) {
-
 		if (player == null) throw new IllegalArgumentException("Player cannot be null.");
 		if (pick == null) throw new IllegalArgumentException("Pick cannot be negative.");
 		if (bet < 0) throw new IllegalArgumentException("Bet cannot be negative.");
