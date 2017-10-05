9e640e945900981bafc9636b9a4adce43346c066
muhammadjunaidiftikhar@gmail.com <alpha861>
2017-10-05 05:59:37 +0500

    Bug # 6

--- src/src/Main.java
+++ src/src/Main.java
@@ -26,6 +26,7 @@ public class Main {
                 int balance = 100;
 
                 int limit = 0;
+
                 player = new Player(name, balance);
                 player.setLimit(limit);
                 int bet = 5;
