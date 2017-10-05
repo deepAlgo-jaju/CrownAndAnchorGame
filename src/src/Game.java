2540a53f72a8964b4eb582e267f8ae6d37e92c3d
muhammadjunaidiftikhar@gmail.com <alpha861>
2017-10-05 05:23:38 +0500

    Bug # 4

--- src/src/Game.java
+++ src/src/Game.java
@@ -9,7 +9,7 @@ public class Game {
 
 	public Game(Dice die1, Dice die2, Dice die3) {
 
-			if (die1 == null || die2 == null || die3 == null) throw new IllegalArgumentException("Dice cannot be null.");
+		if (die1 == null || die2 == null || die3 == null) throw new IllegalArgumentException("Dice cannot be null.");
 		dice = new ArrayList<Dice>();
 		dice.add(die1);
 		dice.add(die2);
