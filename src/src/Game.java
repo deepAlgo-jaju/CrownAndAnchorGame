c34c9faf5a378216d5ab463540ad9cb5a5364389
muhammadjunaidiftikhar@gmail.com <alpha861>
2017-10-05 05:41:16 +0500

    Bug # 5

--- src/src/Game.java
+++ src/src/Game.java
@@ -31,6 +31,7 @@ public class Game {
 		if (bet < 0) throw new IllegalArgumentException("Bet cannot be negative.");
 
 		player.takeBet(bet);
+
 		int matches = 0;
 		for ( Dice d : dice) {
 			d.roll();
