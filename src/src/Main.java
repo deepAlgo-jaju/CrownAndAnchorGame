c6dc094c14870d8528fdd0541b30d82b36b4ade1
muhammadjunaidiftikhar@gmail.com <alpha861>
2017-10-05 06:42:44 +0500

    Bug # 7

--- src/src/Main.java
+++ src/src/Main.java
@@ -15,6 +15,7 @@ public class Main {
         int totalWins = 0;
         int totalLosses = 0;
         //Asking age from the user
+        System.out.println("Enter you age:");
         System.out.println("Enter you age: ");
         while (true)
         {
