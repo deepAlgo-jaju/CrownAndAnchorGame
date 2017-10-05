7af8758d4585e2a20a17ab974b391c59a51362a6
muhammadjunaidiftikhar@gmail.com <alpha861>
2017-10-05 06:00:08 +0500

    Bug # 6

--- src/src/Main.java
+++ src/src/Main.java
@@ -37,7 +37,6 @@ public class Main {
 
                 System.out.println(String.format("%s starts with balance %d, limit %d",
                 		player.getName(), player.getBalance(), player.getLimit()));
-
                 int turn = 0;
                 while (player.balanceExceedsLimitBy(bet) && player.getBalance() < 200)
                 {
