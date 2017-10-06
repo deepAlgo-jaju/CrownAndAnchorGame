9f3ef36a41079927a36296c673aca50707e75e36
muhammadjunaidiftikhar@gmail.com <alpha861>
2017-10-06 06:01:04 +0500

    Bug # 8

--- src/src/Main.java
+++ src/src/Main.java
@@ -102,7 +102,7 @@ public class Main {
                     cdv.get(i).toString().equals("Spade")){
 	            success = true;
             }
-            else
+            else success
             if(success)
         }
 	    return true;
