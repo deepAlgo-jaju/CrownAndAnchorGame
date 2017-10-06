376eee3f924616adf653e00ab7e5e9a82213be6e
muhammadjunaidiftikhar@gmail.com <alpha861>
2017-10-06 05:56:53 +0500

    Bug # 8

--- src/src/Main.java
+++ src/src/Main.java
@@ -96,7 +96,7 @@ public class Main {
 	        if(cdv.get(i).toString().equals("Anchor") ||
                     cdv.get(i).toString().equals("Crown") ||
                     cdv.get(i).toString().equals("Heart") ||
-                    cdv.get(i)){
+                    cdv.get(i).){
 	            return true;
             }
 
