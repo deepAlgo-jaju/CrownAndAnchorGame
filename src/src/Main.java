907a53d0a1b5112bfd6fa4ca773fc7855f93fdc1
muhammadjunaidiftikhar@gmail.com <alpha861>
2017-10-06 05:55:38 +0500

    Bug # 8

--- src/src/Main.java
+++ src/src/Main.java
@@ -95,7 +95,7 @@ public class Main {
 	    for (int i = 0; i <cdv.size(); i++){
 	        if(cdv.get(i).toString().equals("Anchor") ||
                     cdv.get(i).toString().equals("Crown")
-                    cdv.get(i)){
+                    cdv.get(i).toString()){
 	            return true;
             }
 
