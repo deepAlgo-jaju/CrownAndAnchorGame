abea0a0fca1956669b5cb07b4906aa049f784d5f
muhammadjunaidiftikhar@gmail.com <alpha861>
2017-10-06 06:00:31 +0500

    Bug # 8

--- src/src/Main.java
+++ src/src/Main.java
@@ -92,7 +92,7 @@ public class Main {
 	}
 
     private static boolean checkNoSpinThrow(List<DiceValue> cdv) {
-	    boolean success
+	    boolean success = false;
 	    for (int i = 0; i <cdv.size(); i++){
 	        if(cdv.get(i).toString().equals("Anchor") ||
                     cdv.get(i).toString().equals("Crown") ||
