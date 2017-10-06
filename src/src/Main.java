cad5a324737cdc195c4060c0d3a221353edd5a02
muhammadjunaidiftikhar@gmail.com <alpha861>
2017-10-06 06:05:11 +0500

    Bug # 8

--- src/src/Main.java
+++ src/src/Main.java
@@ -71,6 +71,8 @@ public class Main {
                                     player.getName(), player.getBalance());
                             loseCount++;
                         }
+                    }else {
+
                     }
                     
                 } //while
